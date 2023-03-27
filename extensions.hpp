2e7087107f769ec0cbc9c545d9154f4512578e9f
dragoncoder047 <101021094+dragoncoder047@users.noreply.github.com>
2023-03-27 18:43:17 -0400

    indenting bad

--- extensions.hpp
+++ extensions.hpp
@@ -7,23 +7,23 @@
 
 // Definitions
 object* fn_now (object* args, object* env) {
-   (void) env;
-   static unsigned long Offset;
-   unsigned long now = millis()/1000;
-   int nargs = listlength(args);
+    (void) env;
+    static unsigned long Offset;
+    unsigned long now = millis()/1000;
+    int nargs = listlength(args);
 
-   // Set time
-   if (nargs == 3) {
-       Offset = (unsigned long)((checkinteger(first(args))*60 + checkinteger(second(args)))*60
-         + checkinteger(third(args)) - now);
-   } else if (nargs > 0) error2(PSTR("wrong number of arguments"));
+    // Set time
+    if (nargs == 3) {
+        Offset = (unsigned long)((checkinteger(first(args))*60 + checkinteger(second(args)))*60
+            + checkinteger(third(args)) - now);
+    } else if (nargs > 0) error2(PSTR("wrong number of arguments"));
 
-   // Return time
-   unsigned long secs = Offset + now;
-   object* seconds = number(secs%60);
-   object* minutes = number((secs/60)%60);
-   object* hours = number((secs/3600)%24);
-   return cons(hours, cons(minutes, cons(seconds, nil)));
+    // Return time
+    unsigned long secs = Offset + now;
+    object* seconds = number(secs%60);
+    object* minutes = number((secs/60)%60);
+    object* hours = number((secs/3600)%24);
+    return cons(hours, cons(minutes, cons(seconds, nil)));
 }
 
 // Symbol names
