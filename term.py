680d740997b8fdf6e61cae33e60e14fe87584bfe
dragoncoder047 <101021094+dragoncoder047@users.noreply.github.com>
2023-03-31 18:26:21 -0400

    basic asyncio loop

--- term.py
+++ term.py
@@ -1,6 +1,6 @@
 #! /usr/bin/env python3
 from sys import exit
-from serial import Serial, SerialException
+from serial import Serial
 from argparse import ArgumentParser
 from prompt_toolkit import PromptSession, Application
 from prompt_toolkit.layout import VSplit, HSplit, BufferControl, Window
@@ -17,6 +17,7 @@ from prompt_toolkit.shortcuts import set_title
 from pygments.lexers.lisp import CommonLispLexer
 import re
 import os.path
+import asyncio
 
 
 WORKSPACESIZE = 9216 - 172
@@ -41,8 +42,37 @@ class LispValidator(Validator):
             raise ValidationError(len(document.text), "Unclosed string")
 
 
+lispbuffer = TextArea(
+    multiline=True,
+    lexer=PygmentsLexer(CommonLispLexer),
+    focus_on_click=True,
+    scrollbar=True,
+    validator=LispValidator(),
+)
+terminal = TextArea(
+    read_only=True,
+    scrollbar=True,
+    wrap_lines=False,
+)
+app = Application(Layout(HSplit([
+    VSplit([
+        Button(text="Quit", handler=lambda: app.exit(),
+               left_symbol="[", right_symbol="]"),
+        Button(text="Break", handler=lambda: port.write(b"~"),
+               left_symbol="[", right_symbol="]"),
+        Button(text="Reboot", handler=None,
+               left_symbol="[", right_symbol="]")
+    ]),
+    VSplit([
+        lispbuffer,
+        VerticalLine(),
+        terminal
+    ]),
+])), mouse_support=True, full_screen=True)
+
+
 def parse_prompt(prompt: str) -> int:
-    return int(PROMPT_RE.match(prompt) or "0")
+    return int(PROMPT_RE.search(prompt) or "0")
 
 
 def mem_usage_indicator(num_used: int, ps: PromptSession):
@@ -113,54 +143,17 @@ async def repl(port: Serial, ps: PromptSession):
             prompt = passthrough_until_prompt(port).rsplit("\n", 1)[-1]
 
 
-def main():
+async def main():
     a = ArgumentParser("term.py")
     a.add_argument("-p", "--port", default="/dev/ttyUSB0")
     a.add_argument("-b", "--baud", default=115200)
-    a.add_argument("-r", "--histfile", default="~/.ulisp_history")
     x = a.parse_args()
-    try:
-        port = Serial(x.port, x.baud, timeout=0.1, exclusive=True)
-    except SerialException as e:
-        exit(repr(e))
-    x.histfile = os.path.expanduser(x.histfile)
-    if not os.path.exists(x.histfile):
-        with open(x.histfile, "w"):
-            pass
+    port = Serial(x.port, x.baud, timeout=0.1, exclusive=True)
 
-    lispbuffer = TextArea(
-        multiline=True,
-        lexer=PygmentsLexer(CommonLispLexer),
-        history=FileHistory(x.histfile),
-        focus_on_click=True,
-        auto_suggest=AutoSuggestFromHistory(),
-        
-        scrollbar=True,
-        validator=LispValidator(),
-    )
-    terminal = TextArea(
-        read_only=True,
-        scrollbar=True,
-        wrap_lines=False,
-    )
-    terminal.text = "foofoo"
-    app = Application(Layout(HSplit([
-        VSplit([
-            Button(text="Quit", handler=lambda: app.exit(),
-                   left_symbol="[", right_symbol="]"),
-            Button(text="Break", handler=lambda: port.write(b"~"),
-                   left_symbol="[", right_symbol="]"),
-            Button(text="Reboot", handler=None,
-                   left_symbol="[", right_symbol="]")
-        ]),
-        VSplit([
-            lispbuffer,
-            VerticalLine(),
-            terminal
-        ]),
-    ])), mouse_support=True, full_screen=True)
-    app.run()
+    tasks = []
+    tasks.append(app.run_async())
+    await asyncio.gather(*tasks)
 
 
 if __name__ == '__main__':
-    main()
+    asyncio.run(main())
